ca7e5edb1059dc7d34ee728c9da63d8813d60e8f
itz-kap304 <2019ume0201@iitjammu.ac.in>
2023-03-08 00:25:17 +0530

    Basic File Structure Made

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import BucketList from './components/BucketList'
+import Card from './components/Card'
+import CardList from './components/CardList'
+import History from './components/History'
+import Modal from './components/Modal'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
